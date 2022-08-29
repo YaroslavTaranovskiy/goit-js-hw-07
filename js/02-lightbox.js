57cf8656aaa8fcba8fecd0de865bbdeef459c42d
YaroslavTaranovskiy <103592817+YaroslavTaranovskiy@users.noreply.github.com>
2022-08-29 12:16:03 +0300

    task-2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,28 +6,27 @@ const cardsMarkup = createGalleryCardsMarkup(galleryItems);
 imgContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
 function createGalleryCardsMarkup(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
-
+  return galleryItems
+    .map(({ preview, original, description }) => {
         return `
-        <a class="gallery__item" 
-        href="${original}">
-  <img class="gallery__image" 
-  src="${preview}" 
-  alt="${description}" 
-  />
-</a>
+            <a class="gallery__item" 
+                href="${original}">
+                <img class="gallery__image" 
+                src="${preview}" 
+                alt="${description}" 
+                />
+            </a>
         `;
     })
-        .join('');
+    .join('');
 }
 
 var lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: "alt",
-    captionDelay: 100,
-    captionPosition: 'bottom',
-    // nav: true,
-    // close: true,
-    // caption: true,
-    
+  captionsData: 'alt',
+  captionDelay: 100,
+  captionPosition: 'bottom',
+  // nav: true,
+  // close: true,
+  // caption: true,
 });
 console.log(galleryItems);
